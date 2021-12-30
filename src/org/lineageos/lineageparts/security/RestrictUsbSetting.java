82a881c0f4fa7c50ec0a95bd9dd8ed490d20314e
Chirayu Desai <chirayudesai1@gmail.com>
2021-12-30 17:27:14 +0530

    Add standlone "Restrict USB" setting for Settings -> Security
    * Based on Trust
    * It's now a ListPreference instead of a SwitchPreference though
    
    Change-Id: I6d9fffd8b6766b90910f2040597f5fc204b6c811

---
+++ src/org/lineageos/lineageparts/security/RestrictUsbSetting.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2018-2020 The LineageOS Project
+ * Copyright (C) 2021 The Calyx Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.lineageos.lineageparts.security;
+
+import android.os.Bundle;
+
+import androidx.preference.Preference;
+import androidx.preference.ListPreference;
+
+import lineageos.trust.TrustInterface;
+import lineageos.providers.LineageSettings;
+
+import org.lineageos.lineageparts.R;
+import org.lineageos.lineageparts.SettingsPreferenceFragment;
+
+public class RestrictUsbSetting extends SettingsPreferenceFragment {
+    private static final String TAG = "RestrictUsbSetting}";
+
+    private ListPreference mUsbRestrictorPref;
+
+    private TrustInterface mInterface;
+
+    @Override
+    public void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+
+        mInterface = TrustInterface.getInstance(getContext());
+
+        addPreferencesFromResource(R.xml.restrict_usb_setting);
+        mUsbRestrictorPref = findPreference("security_restrict_usb");
+        mUsbRestrictorPref.setOnPreferenceChangeListener((p, v) ->
+                onUsbRestrictorPrefChanged(Integer.parseInt((String) v)));
+        setup();
+    }
+
+    private void setup() {
+        if (!mInterface.hasUsbRestrictor()) {
+            //removePreference(mUsbRestrictorPref);
+        }
+    }
+
+    private boolean onUsbRestrictorPrefChanged(Integer value) {
+        LineageSettings.Global.putInt(getContext().getContentResolver(),
+                LineageSettings.Global.TRUST_RESTRICT_USB, value);
+        return true;
+    }
+}

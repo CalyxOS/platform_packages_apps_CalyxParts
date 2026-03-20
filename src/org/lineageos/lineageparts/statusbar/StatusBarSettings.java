5c20a4ea84fc9eb859264493089992f6a8c8bf32
Michael Bestas <mkbestas@gmail.com>
2026-03-20 20:40:11 +0200

    Remove status bar brightness customizations
    Since 16 QPR1, QS has moved to compose and adding the LineageOS
    brightness customizations is very intrusive and prone to conflicts.
    
    Change-Id: I819ddfc901269dfde2ad49a71c6302835dddb759

--- src/org/lineageos/lineageparts/statusbar/StatusBarSettings.java
+++ src/org/lineageos/lineageparts/statusbar/StatusBarSettings.java
@@ -10,8 +10,6 @@ import static org.lineageos.lineageparts.utils.ResourceUtils.isRtlMode;
 import android.content.res.Resources;
 import android.os.Bundle;
 
-import lineageos.preference.LineageSecureSettingListPreference;
-import lineageos.preference.LineageSecureSettingSwitchPreference;
 import lineageos.preference.LineageSystemSettingListPreference;
 
 import org.lineageos.lineageparts.R;
@@ -19,36 +17,18 @@ import org.lineageos.lineageparts.SettingsPreferenceFragment;
 
 public class StatusBarSettings extends SettingsPreferenceFragment {
 
-    private static final String QS_BRIGHTNESS_SLIDER_POSITION = "qs_brightness_slider_position";
-    private static final String QS_SHOW_AUTO_BRIGHTNESS = "qs_show_auto_brightness";
-    private static final String QS_SHOW_BRIGHTNESS_SLIDER = "qs_show_brightness_slider";
-
     private static final String STATUS_BAR_QUICK_QS_PULLDOWN = "qs_quick_pulldown";
-
-    private static final int QS_BRIGHTNESS_SLIDER_HIDDEN = 0;
 
     private static final int PULLDOWN_DIR_NONE = 0;
     private static final int PULLDOWN_DIR_RIGHT = 1;
     private static final int PULLDOWN_DIR_LEFT = 2;
 
-    private LineageSecureSettingListPreference mQsBrightnessSliderPosition;
-    private LineageSecureSettingSwitchPreference mQsShowAutoBrightness;
     private LineageSystemSettingListPreference mQuickPulldown;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.status_bar_settings);
-
-        mQsShowAutoBrightness = findPreference(QS_SHOW_AUTO_BRIGHTNESS);
-        mQsBrightnessSliderPosition = findPreference(QS_BRIGHTNESS_SLIDER_POSITION);
-        LineageSecureSettingListPreference qsShowBrightnessSlider =
-                findPreference(QS_SHOW_BRIGHTNESS_SLIDER);
-        qsShowBrightnessSlider.setOnPreferenceChangeListener((preference, newValue) -> {
-            enableQuickSettingsBrightnessSliderDependents(Integer.parseInt((String) newValue));
-            return true;
-        });
-        enableQuickSettingsBrightnessSliderDependents(qsShowBrightnessSlider.getIntValue(1));
 
         mQuickPulldown = findPreference(STATUS_BAR_QUICK_QS_PULLDOWN);
         mQuickPulldown.setSummaryProvider(preference -> {
@@ -82,12 +62,5 @@ public class StatusBarSettings extends SettingsPreferenceFragment {
         } else {
             mQuickPulldown.setEntries(R.array.status_bar_quick_qs_pulldown_entries);
         }
-    }
-
-    private void enableQuickSettingsBrightnessSliderDependents(int showBrightnessSlider) {
-        boolean enabled = showBrightnessSlider != QS_BRIGHTNESS_SLIDER_HIDDEN;
-
-        mQsBrightnessSliderPosition.setEnabled(enabled);
-        mQsShowAutoBrightness.setEnabled(enabled);
     }
 }
